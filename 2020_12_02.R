72763ebb6ce72efb84c2de0a9c462179c4770d7f
unknown <dnverry@googlemail.com>
2021-11-25 22:29:23 +0000

    Correct answer for 2020-12-02

---
+++ 2020_12_02.R
@@ -0,0 +1,20 @@
+# December 2nd 2020
+
+library(tidyverse)
+
+data  = read.csv("input_2020_12_02.txt", header = FALSE, sep = " ")
+
+answer = data %>%
+  select(rule = V1, character = V2, password = V3) %>%
+  mutate(min = as.numeric(sub("-.*", "", rule)),
+         max = as.numeric(sub(".*-", "", rule)),
+         character = str_remove(character, ":"),
+         min_test = str_count(password, character) >= min,
+         max_test = str_count(password, character) <= max) %>%
+  filter(min_test == TRUE, max_test == TRUE) %>%
+  nrow()
+
+answer
+
+
+#Works but I'm sure this could be done in 2 lines
