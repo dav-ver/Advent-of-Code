be59d34bd0b6f89d6f3b0f414834812f2611045a
unknown <dnverry@googlemail.com>
2021-11-25 22:50:47 +0000

    Part 2 answered

--- 2020/2020_12_02.R
+++ 2020/2020_12_02.R
@@ -19,3 +19,24 @@ answer
 
 
 #Works but I'm sure this could be done in 2 lines
+
+
+
+## PART 2
+
+answer_2 = data %>%
+  select(rule = V1, character = V2, password = V3) %>%
+  # Inspiration: https://statisticsglobe.com/extract-substring-before-or-after-pattern-in-r
+  mutate(position_1 = as.numeric(sub("-.*", "", rule)),
+         position_2 = as.numeric(sub(".*-", "", rule)),
+         character = str_remove(character, ":"),
+         position_1_test = str_sub(password, position_1, position_1) == character,
+         position_2_test = str_sub(password, position_2, position_2) == character) %>%
+  filter((position_1_test == TRUE | position_2_test == TRUE),
+         position_1_test != position_2_test) %>%
+  nrow()
+
+answer_2
+
+#Again, this works but it's super long and ugly
+  
