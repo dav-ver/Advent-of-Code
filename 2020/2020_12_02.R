e2060a20de1e82dc2b6950afdb1fe00401b7e770
unknown <dnverry@googlemail.com>
2021-11-25 22:35:19 +0000

    Moved files to 2020 folder

--- 2020/2020_12_02.R
+++ 2020/2020_12_02.R
@@ -6,6 +6,7 @@ data  = read.csv("input_2020_12_02.txt", header = FALSE, sep = " ")
 
 answer = data %>%
   select(rule = V1, character = V2, password = V3) %>%
+  # Inspiration: https://statisticsglobe.com/extract-substring-before-or-after-pattern-in-r
   mutate(min = as.numeric(sub("-.*", "", rule)),
          max = as.numeric(sub(".*-", "", rule)),
          character = str_remove(character, ":"),
