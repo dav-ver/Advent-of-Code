d84ce84bc11d43f6041756fa5dedaef3f92f3c09
unknown <dnverry@googlemail.com>
2021-12-03 23:37:08 +0000

    Part 1 working, part 2 not

---
+++ 2021/2021_12_03.R
@@ -0,0 +1,92 @@
+## December 03 2021 - completed with Katie and Bea
+
+library(tidyverse)
+library(assertr)
+
+data = read.fwf(file = "~/R_projects/Advent-of-Code/2021/input_2021_12_03.txt", width = rep(1, 12))
+
+answer = data %>%
+  summarise_all(mean) %>%
+  round() %>%
+  bind_rows(1 - .) %>%
+  mutate(number = col_concat(.)) %>%
+  select(number) %>%
+  mutate(number = strtoi(number, 2)) %>%
+  prod()
+
+answer
+
+
+
+## PART 2
+
+data_concat = data %>%
+  mutate(number = col_concat(.))
+
+data2 = data_concat
+for (i in 1:12) {
+  if (sum(data2[[i]]) == length(data2[[i]])/2) {
+    data2 = data2 %>%
+      dplyr::filter(data2[[i]] == 1)
+  }
+  else {
+    data2 = data2 %>%
+      dplyr::filter(data2[[i]] == round(mean(data2[[i]])))
+  }
+  if (length(data2[[1]]) == 1) {
+    break
+  }
+}
+
+data3 = data_concat
+for (i in 1:12) {
+  if (sum(data3[[i]]) == length(data3[[i]])/2) {
+    data3 = data3 %>%
+      dplyr::filter(data3[[i]] == 0)
+  }
+  else {
+    data3 = data3 %>%
+      dplyr::filter(data3[[i]] == round(mean(data3[[i]])))
+  }
+  if (length(data3[[1]]) == 1) {
+    break
+  }
+}
+
+answer2 = data2 %>%
+  bind_rows(data3) %>%
+  select(number) %>%
+  mutate(number = strtoi(number, 2)) %>%
+  prod()
+
+answer2
+
+
+#attempt 2
+
+data_test = data %>%
+  bind_rows(data %>% summarise_all(.funs = ~round(mean(.))))
+
+keep_mode = function(data, column, tie_result) {
+  col_sum = sum(column)
+  col_length = length(column)
+  if (2*col_sum == col_length) {
+    col_mode = tie_result
+  }
+  else {
+    col_mode = round(col_sum/col_length)
+  }
+  data = data %>%
+    filter(column == col_mode)
+  return(data)
+}
+
+data_concat_o2 = data_concat
+for (i in 1:12) {
+  data_concat_o2 = keep_mode(data_concat_o2, data_concat_o2[[i]], 1)
+}
+
+data_concat_co2 = data_concat
+for (i in 1:12) {
+  data_concat_co2 = keep_mode(data_concat_co2, data_concat_co2[[i]], 1)
+}
